e5e3dee4179d0d56a9f285d45871d4353ceab237
gunheeyoon <gunheeyoon@outlook.com>
2022-04-12 23:23:35 +0900

    having more funs

--- swifty/main.swift
+++ swifty/main.swift
@@ -39,3 +39,34 @@ for (num, numbers) in interestingNumbers {
 }
 
 print(largest)
+
+//function without label uses _
+//put custom label in front of the actual label
+
+func greet(_ person: String, on day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+
+print(greet("David", on: "Friday"))
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    
+    return (min, max, sum)
+}
+
+let statistics = calculateStatistics(scores: [6, 8, 100, 30, 12])
+print(statistics.sum)
+print(statistics.2) //same as sum
+print(statistics)
